5a1d2d3208529e8f32863d18810886010437dc91
hesisi <1004858818@qq.com>
2022-08-01 11:18:15 +0800

    fix: 去掉多余以来

--- config/config.ts
+++ config/config.ts
@@ -4,7 +4,7 @@
  * @Author: hesisi
  * @Date: 2022-06-13 16:09:41
  * @LastEditors: hesisi
- * @LastEditTime: 2022-07-22 15:17:27
+ * @LastEditTime: 2022-08-01 11:16:35
  */
 import { defineConfig } from 'umi';
 import routes from './routes.js';
@@ -15,15 +15,6 @@ export default defineConfig({
   },
   // mode: 'history',//去掉#号
   routes,
-  // copy: [{
-  //   form: 'lib',
-  //   to: 'lib'
-  // }],
-  // headScripts: [
-  //   {src: './lib/ueditor/ueditor.config.js'},
-  //   {src: './lib/ueditor/ueditor.all.js'},
-  //   {src: './lib/ueditor/lang/zh-cn/zh-cn.js'}
-  // ],
   fastRefresh: {},
-  publicPath: '/',
+  // publicPath: '/',
 });

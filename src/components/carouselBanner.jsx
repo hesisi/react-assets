b108f18f3c339aad3774b866b49fa677b633a236
He Hechuanbao (FG-CN-4) <1226776085@qq.com>
2022-08-29 16:51:15 +0800

    轮播图ui修改

--- src/components/carouselBanner.jsx
+++ src/components/carouselBanner.jsx
@@ -1,31 +1,35 @@
 import { Carousel } from 'antd';
-import React from 'react';
-// const banner1 = require('../assets/pic.jpg');
-// const banner2 = require('../assets/test.jpg');
-// const banner3 = require('../assets/panda.jpg');
+import React, { Component } from 'react';
 const banner1 = require('../assets/picture.webp');
 const banner2 = require('../assets/picture2.webp');
 const banner3 = require('../assets/picture3.webp');
 const banner = [banner1, banner2, banner3];
-const contentStyle = {
-  height: '100%',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
-const CarouselBanner = (props) => (
-  <div className={props.className}>
-    <Carousel autoplay>
-      {banner.map((e, i) => {
-        return (
-          <div style={{ height: '100%' }} key={i}>
-            <img src={e} />
-          </div>
-        );
-      })}
-    </Carousel>
-  </div>
-);
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import './public.less';
 
-export default CarouselBanner;
+export default class carouselBanner extends Component {
+  handleChange = (data) => {
+    data === 'left' ? this.carouselBanner.prev() : this.carouselBanner.next();
+  };
+  render() {
+    return (
+      <div style={{ position: 'relative' }} className={this.props.className}>
+        <p className="left" onClick={() => this.handleChange('left')}>
+          <LeftOutlined />
+        </p>
+        <p className="right" onClick={() => this.handleChange('right')}>
+          <RightOutlined />
+        </p>
+        <Carousel autoplay ref={(c) => (this.carouselBanner = c)}>
+          {banner.map((e, i) => {
+            return (
+              <div style={{ height: '100%' }} key={i}>
+                <img src={e} />
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
+    );
+  }
+}

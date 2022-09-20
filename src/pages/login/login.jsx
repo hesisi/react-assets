8d845f1c5db3e53c7ec46c3902676773d70e42a3
yilzhao <yilzhao@deloitte.com.cn>
2022-09-20 15:20:25 +0800

    feat 登录修改

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -26,27 +26,27 @@ const Login = () => {
       ...values,
       uuid: validateUuid,
     };
-    userLogin(params)
-      .then((res) => {
-        if (res?.data?.isSuccess > 0) {
-          const { data } = res.data;
-          window.localStorage.setItem('loginToken', data.token);
-          window.localStorage.setItem('username', params.username) || '';
-          if (!data.isReset) {
-            history.push('resetPassword');
-          } else {
-            history.push('homeIndex');
-          }
+    userLogin(params).then((res) => {
+      if (res?.data?.isSuccess > 0) {
+        const { data } = res.data;
+        window.localStorage.setItem('loginToken', data.token);
+        window.localStorage.setItem('username', params.username) || '';
+        if (!data.isReset) {
+          history.push('resetPassword');
+        } else {
+          history.push('homeIndex');
         }
-        //  else {
-        //   message.error(res?.data?.message);
-        // }
-      })
-      .catch(() => {
+      } else {
         // 报错时，重新请求验证码
         form.setFieldValue('code', '');
         getValidatePic();
-      });
+      }
+    });
+    // .catch(() => {
+    //   // 报错时，重新请求验证码
+    //   form.setFieldValue('code', '');
+    //   getValidatePic();
+    // });
   };
   const changeShowLabel = (lable) => {
     if (showLabel === lable && lable === 'username') {

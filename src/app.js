bfaee8be83ded775579e31ef0ba6461873e70ffa
lisisi <sisli@deloitte.com.cn>
2022-09-21 18:23:09 +0800

    fix:[菜单]icon回显

--- src/app.js
+++ src/app.js
@@ -4,22 +4,10 @@ import { getMenuList } from '@/services/menu';
 export function render(oldRender) {
   const isLogin = window.localStorage.getItem('loginToken');
   if (isLogin) {
-    getMenuList()
-      .then(async (res) => {
-        // console.log('callback', res);
-        try {
-          await localForage.setItem(
-            'menuTreePermission',
-            res?.data?.data || [],
-          );
-          oldRender && oldRender();
-        } catch {
-          oldRender && oldRender();
-        }
-      })
-      .finally(() => {
-        oldRender && oldRender();
-      });
+    getMenuList().then(async (res) => {
+      await localForage.setItem('menuTreePermission', res?.data?.data || []);
+      oldRender && oldRender();
+    });
   } else {
     oldRender && oldRender();
   }

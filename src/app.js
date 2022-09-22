7d8c2b18273eaa02c4eecd43070da7d42a6059c2
lisisi <sisli@deloitte.com.cn>
2022-09-22 09:33:09 +0800

    fix:[菜单]

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ import { getMenuList } from '@/services/menu';
 export function render(oldRender) {
   const isLogin = window.localStorage.getItem('loginToken');
   if (isLogin) {
-    getMenuList().then(async (res) => {
-      await localForage.setItem('menuTreePermission', res?.data?.data || []);
-      oldRender && oldRender();
-    });
+    getMenuList()
+      .then(async (res) => {
+        await localForage.setItem('menuTreePermission', res?.data?.data || []);
+        oldRender && oldRender();
+      })
+      .finally(() => {
+        oldRender && oldRender();
+      });
   } else {
     oldRender && oldRender();
   }
